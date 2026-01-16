4ab059fb9095637c7599900bc57d8a0b973ce4ac
Fitrotul Faizah <151208570+things2103@users.noreply.github.com>
2026-01-16 22:08:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # app.py
+# -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
 import joblib
 import re, unicodedata
 import nltk
 from nltk.corpus import stopwords
-from sklearn.base import BaseEstimator, TransformerMixin
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from sklearn.base import BaseEstimator, TransformerMixin
 
 # =======================
 # Konfigurasi halaman
@@ -27,10 +28,56 @@ st.write("Pilih CV, model, masukkan teks email, lalu prediksi spam/ham.")
 nltk.download('stopwords')
 
 # =======================
-# Custom Preprocessing Class
+# Load Kamus Gaul & Stopwords
 # =======================
+df_kamus = pd.read_csv("kamus_gaul.csv")  # sesuaikan path
+kamus_normalisasi = dict(zip(df_kamus['slang'], df_kamus['formal']))
+kamus_normalisasi.update({
+    "communications": "komunikasi",
+    "university": "universitas",
+    "mail": "email",
+    "file": "berkas"
+})
+
+stop_factory = StopWordRemoverFactory()
+stopwords_id = set(stop_factory.get_stop_words())
+stopwords_en = set(stopwords.words('english'))
+
+additional_stopwords = {
+    "gue", "viagra", "per",
+    "dpc", "nya", "sih",
+    "gas", "pana", "corp", "rice", "london","dear",
+    "faks", "ees", "lon", "jul","rxoo","gelling","epa"
+}
+
+stopwords_all = stopwords_id.union(stopwords_en).union(additional_stopwords)
+
+stemmer_factory = StemmerFactory()
+stemmer = stemmer_factory.create_stemmer()
+
+# =======================
+# Preprocessing
+# =======================
+def clean_text_model(text):
+    text = text.replace("\n", " ").replace("\r", " ")
+    text = str(text).lower()
+    text = re.sub(r'\b(?:https?://|www\.|http\b|https\b)\S*|\S+@\S+\b', ' ', text)
+    text = re.sub(r'\b(from|to|cc|bcc|subject|subjek|re|fw|fwd)\b', ' ', text)
+    text = re.sub(r'\b(com|net|org|id|edu|inc|co)\b', ' ', text)
+    text = re.sub(r'\b(enron|kaminski|vince|shirley|stinson|houston|hice|adobe|john|david|hou|crenshaw|stanford|ect|ee|eb|etc)\b', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'[_\-‐-―]+', ' ', text)
+    text = ''.join(ch for ch in text if not unicodedata.category(ch).startswith("C"))
+    text = re.sub(r'[^a-z\s]', ' ', text)
+    text = re.sub(r'(.)\1{2,}', r'\1', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    words = text.split()
+    words = [kamus_normalisasi.get(w, w) for w in words]
+    words = [stemmer.stem(w) for w in words]
+    words = [w for w in words if w not in stopwords_all and len(w) > 2]
+    return " ".join(words)
+
 class TextPreprocessor(BaseEstimator, TransformerMixin):
-    def __init__(self, kamus_normalisasi=None, stopwords_all=None, stemmer=None):
+    def __init__(self, kamus_normalisasi, stopwords_all, stemmer):
         self.kamus_normalisasi = kamus_normalisasi
         self.stopwords_all = stopwords_all
         self.stemmer = stemmer
@@ -39,53 +86,14 @@ class TextPreprocessor(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        return [self._clean_text(x) for x in X]
+        return [clean_text_model(str(x)) for x in X]
 
-    def _clean_text(self, text):
-        text = str(text).lower()
-        text = text.replace("\n", " ").replace("\r", " ")
-        text = re.sub(r'\b(?:https?://|www\.|http\b|https\b)\S*|\S+@\S+\b', ' ', text)
-        text = re.sub(r'\b(from|to|cc|bcc|subject|subjek|re|fw|fwd)\b', ' ', text)
-        text = re.sub(r'\b(com|net|org|id|edu|inc|co)\b', ' ', text)
-        text = re.sub(r'[_\-‐-―]+', ' ', text)
-        text = ''.join(ch for ch in text if not unicodedata.category(ch).startswith("C"))
-        text = re.sub(r'[^a-z\s]', ' ', text)
-        text = re.sub(r'(.)\1{2,}', r'\1', text)
-        text = re.sub(r'\s+', ' ', text).strip()
-        words = text.split()
-        if self.kamus_normalisasi:
-            words = [self.kamus_normalisasi.get(w, w) for w in words]
-        if self.stemmer:
-            words = [self.stemmer.stem(w) for w in words]
-        if self.stopwords_all:
-            words = [w for w in words if w not in self.stopwords_all and len(w) > 2]
-        return " ".join(words)
+preprocess = TextPreprocessor(kamus_normalisasi, stopwords_all, stemmer)
 
 # =======================
-# Load kamus dan stopwords
+# Pilih Model
 # =======================
-stop_factory = StopWordRemoverFactory()
-stopwords_id = set(stop_factory.get_stop_words())
-stopwords_en = set(stopwords.words('english'))
-
-stemmer_factory = StemmerFactory()
-stemmer = stemmer_factory.create_stemmer()
-
-df_kamus = pd.read_csv("kamus_gaul.csv")
-kamus_normalisasi = dict(zip(df_kamus['slang'], df_kamus['formal']))
-
-additional_stopwords = {
-    "gue", "viagra", "per", "dpc", "nya", "sih",
-    "gas", "pana", "corp", "rice", "london","dear",
-    "faks", "ees", "lon", "jul","rxoo","gelling","epa"
-}
-
-stopwords_all = stopwords_id.union(stopwords_en).union(additional_stopwords)
-
-# =======================
-# Pilih CV & Model
-# =======================
-MODEL_DIR = "models/"
+MODEL_DIR = "models/"  # folder model di repo
 
 models_cv3 = {
     "NB Baseline": "cv3_nb_baseline.pkl",
@@ -110,6 +118,7 @@ models_cv5 = {
 }
 
 cv_choice = st.selectbox("Pilih CV:", ["CV 3", "CV 5"])
+
 if cv_choice == "CV 3":
     model_name = st.selectbox("Pilih model:", list(models_cv3.keys()))
     model_file = models_cv3[model_name]
@@ -131,13 +140,14 @@ if st.button("📧 Prediksi Spam/Ham"):
         st.warning("Masukkan teks email terlebih dahulu!")
         st.stop()
 
-    # Load pipeline model lengkap
+    # Load model fit
     model_path = MODEL_DIR + model_file
     model = joblib.load(model_path)
 
     # Prediksi
     texts = [email_input.strip()]
     predictions = model.predict(texts)
+
     try:
         probabilities = model.predict_proba(texts)
     except:
