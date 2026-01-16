9ccec132345f15c34e54cd91e83ddf093e62a08b
Fitrotul Faizah <151208570+things2103@users.noreply.github.com>
2026-01-16 21:43:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,8 +89,12 @@ stemmer = stemmer_factory.create_stemmer()
 
 # Kamus gaul
 # Load CSV kamus gaul
+df_kamus = pd.read_csv("kamus_gaul.csv")
+# Hapus spasi di awal/akhir nama kolom
+df_kamus.columns = df_kamus.columns.str.strip()
+st.write("Kolom CSV kamus_gaul:", df_kamus.columns.tolist())
 df_kamus = pd.read_csv("kamus_gaul.csv", encoding="utf-8")
-kamus_normalisasi = dict(zip(df_kamus['slang   '], df_kamus['formal   ']))
+kamus_normalisasi = dict(zip(df_kamus['slang'], df_kamus['formal']))
 kamus_normalisasi["communications"] = "komunikasi"
 kamus_normalisasi["university"] = "universitas"
 kamus_normalisasi["mail"] = "email"
